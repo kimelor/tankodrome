b6617ac382699b1d4ea60feeedc5e7226c9e914a
Luis Navarro Verduzco <kimelor@gmail.com.mx>
2013-06-14 09:12:04 -0700

    commit de prueva

---
+++ src/resources/CONSTANTS.py
@@ -0,0 +1,12 @@
+'''
+Created on Jun 13, 2013
+
+@author: morhir
+'''
+# - Color Constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (127, 127, 127)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
